74bc80b0cb136c00f7ce3b8a5eedbc508b7427c9
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-03-19 15:29:47 +0000

    Connect meat_fish sheet to meat & fish current stock submenu

--- run.py
+++ run.py
@@ -17,28 +17,28 @@ meat_fish = SHEET.worksheet('meat_fish')
 data = meat_fish.get_all_values()
 
 #print(data)
-print("----------------------------------\n")
+print("----------------------------------\n") #Welcome message
 print("WELCOME TO STOCK-TRACE\n")
 print("----------------------------------\n")
 
-def main_menu():
+def main_menu():   #Main Menu
     print("1. Current Stock")
     print("2. Input New Items")
     print("3. Use Stock Items\n")
 
     
-    print("----------------------------------\n")
-    option = int(input("Please select an option from 1-3: \n"))
-    if option==1:
+    print("----------------------------------\n") 
+    option = int(input("Please select an option from 1-3: \n")) #Input main menu options
+    if option == 1:
         print("Current Stock:")
-        submenu_1()
-    elif option==2:
+        submenu_current()
+    elif option == 2:
         print("Input New Stock Items")
-    elif option ==3:
+    elif option == 3:
         print("Use Stock Items")
-    else: print("Invalid Choice. Please enter a number between 1 & 3")
+    else: print("Invalid Choice. Please enter a number between 1 & 3") #add letters as invalid choice
     
-def submenu_1():
+def submenu_current(): #Submenu for current stock
     print("1. Meat & Fish")
     print("2. Fruit & Veg")
     print("3. Dry Goods")
@@ -46,9 +46,25 @@ def submenu_1():
     print("5. Frozen Items")
     print("6. Return to Main Menu")
     
+    option = int(input("Please select an option from 1-6: \n")) #Input for submenu current stock
+    if option == 1:
+        print("Meat & Fish")
+        data = meat_fish.get_all_values()
+        for row in data:
+            print(row)
+    elif option == 2:
+        print("Fruit & Veg")
+    elif option == 3:
+        print("Dry Goods")
+    elif option == 4:
+        print("Chilled Goods")
+    elif option == 5:
+        print("Frozen Items")
+    elif option == 6:
+        print("Return to Current Stock Menu")
+    else: print("Invalid Choice. Please enter a number between 1 & 6")
          
 main_menu()
-
 
 
 
