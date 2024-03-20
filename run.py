3015515a8bf1441d68883be86e21badfbde2ef77
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-03-20 17:52:10 +0000

    Add return to main menu, press any key to return to stock menu.

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -13,29 +13,22 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('stock-trace')
 
 meat_fish = SHEET.worksheet('meat_fish')
-data = meat_fish.get_all_values()
 fruit_veg = SHEET.worksheet('fruit_veg')
-data = meat_fish.get_all_values()
 dry_goods = SHEET.worksheet("dry_goods")
-data = dry_goods.get_all_values()
 chilled_goods = SHEET.worksheet('chilled_goods')
-data = chilled_goods.get_all_values()
 frozen_items = SHEET.worksheet('frozen_items')
-data = frozen_items.get_all_values()
 
 #print(data)
-print("----------------------------------\n") #Welcome message
+print("----------------------------------\n") # Welcome message
 print("WELCOME TO STOCK-TRACE\n")
 print("----------------------------------\n")
 
-def main_menu():   #Main Menu
+def main_menu():   # Main Menu
     print("1. Current Stock")
     print("2. Input New Items")
     print("3. Use Stock Items\n")
-
-    
     print("----------------------------------\n") 
-    option = int(input("Please select an option from 1-3: \n")) #Input main menu options
+    option = int(input("Please select an option from 1-3: \n")) # Input main menu options
     if option == 1:
         print("Current Stock:")
         submenu_current()
@@ -44,10 +37,11 @@ def main_menu():   #Main Menu
     elif option == 3:
         print("Use Stock Items")
     else: 
-        print("Invalid Choice. Please enter a number between 1 & 3") #add letters as invalid choice
+        print("Invalid Choice. Please enter a number between 1 & 3") # add letters as invalid choice
     
-def submenu_current(): #Submenu for current stock
+def submenu_current(): # Submenu for current stock
     while True:
+        print("----------------------------------\n")
         print("1. Meat & Fish")
         print("2. Fruit & Veg")
         print("3. Dry Goods")
@@ -55,42 +49,44 @@ def submenu_current(): #Submenu for current stock
         print("5. Frozen Items")
         print("6. Return to Main Menu")
         print("----------------------------------\n")
-    
-        option = int(input("Please select an option from 1-6: \n")) #Input for submenu current stock
+        
+        option = int(input("Please select an option from 1-6: \n")) # Input for submenu current stock
+        
         if option == 1:
             print("Meat & Fish")
             data = meat_fish.get_all_values()
             for row in data:
                 print(row)
+            input("Press any key to return to the Current Stock Menu\n")
         elif option == 2:
             print("Fruit & Veg")
             data = fruit_veg.get_all_values()
             for row in data:
                 print(row)
+            input("Press any key to return to the Current Stock Menu\n")
         elif option == 3:
             print("Dry Goods")
             data = dry_goods.get_all_values()
             for row in data:
                 print(row)
+            input("Press any key to return to the Current Stock Menu\n")
         elif option == 4:
             print("Chilled Goods")
             data = chilled_goods.get_all_values()
             for row in data:
                 print(row)
+            input("Press any key to return to the Current Stock Menu\n")
         elif option == 5:
             print("Frozen Items")
             data = frozen_items.get_all_values()
             for row in data:
                 print(row)
+            input("Press any key to return to the Current Stock Menu\n")
         elif option == 6:
-            print("Return to Main Menu") #check this function for menu being added to list
+            print("Return to Main Menu")
             main_menu()
             break
         else: 
             print("Invalid Choice. Please enter a number between 1 & 6")
          
 main_menu()
-
-
-
-
